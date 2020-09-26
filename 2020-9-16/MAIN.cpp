adf9906618cabb53ebabb2c285fe2091e02a0825
JUSTYNTOWLER <70733723+JUSTYNTOWLER@users.noreply.github.com>
2020-09-26 13:22:59 +0800

    Add files via upload

---
+++ 2020-9-16/MAIN.cpp
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include "四则运算.h"
+#include "判断非法字符.h"
+#include "判断计算2.h"
---
+++ 2020-9-16/三角函数.cpp
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include "Èý½Çº¯Êý.h"
+
+using namespace std;
